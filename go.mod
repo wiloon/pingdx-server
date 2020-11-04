1bd1356ee9af9e9a81c0ea8a10676f210207d949
wiloon <wiloon.wy@gmail.com>
2020-11-04 18:33:37 +0800

    compare comma to rn

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module pingdx-server
 
 go 1.15
 
-require github.com/gin-gonic/gin v1.6.3
+require (
+	github.com/emirpasic/gods v1.12.0
+	github.com/gin-gonic/gin v1.6.3
+)
--- pingdx-server.go
+++ pingdx-server.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"github.com/emirpasic/gods/sets/hashset"
 	"github.com/gin-gonic/gin"
+	"sort"
+	"strings"
 )
 
 type compareParams struct {
 	Lista string `json:"lista"`
 	Listb string `json:"listb"`
+}
+
+type Result struct {
+	OnlyInA      []string
+	OnlyInB      []string
+	Intersection []string
 }
 
 func main() {
@@ -19,15 +29,71 @@ func main() {
 			"message": "pong",
 		})
 	})
+	router.GET("/comma-to-newline", func(c *gin.Context) {
+		foo := c.Query("foo")
+		bar := strings.ReplaceAll(foo, ",", "\n")
+		c.JSON(200, bar)
+	})
+	router.GET("/newline-to-comma", func(c *gin.Context) {
+		bar := c.Query("bar")
+		arr := strings.Split(bar, "\n")
 
+		var foo bytes.Buffer
+		for _, v := range arr {
+			foo.WriteString(strings.TrimSpace(v))
+			foo.WriteString(",")
+		}
+		out := foo.String()
+		c.JSON(200, out[:len(out)-1])
+	})
 	router.POST("/compare", func(c *gin.Context) {
 		params := &compareParams{}
 		_ = c.BindJSON(params)
-		fmt.Println(params)
-		fmt.Println(params.Lista)
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+		fmt.Println("list a: " + params.Lista)
+		arrA := strings.Split(params.Lista, "\n")
+		arrB := strings.Split(params.Listb, "\n")
+
+		setA := hashset.New()
+		for _, v := range arrA {
+			if v != "" {
+				setA.Add(strings.TrimSpace(v))
+			}
+		}
+		setB := hashset.New()
+		for _, v := range arrB {
+			if v != "" {
+				setB.Add(strings.TrimSpace(v))
+			}
+		}
+
+		var resultOnlyInA []string
+		var resultOnlyInB []string
+		var resultIntersection []string
+		for _, v := range arrA {
+			if setB.Contains(v) {
+				resultIntersection = append(resultIntersection, v)
+				setB.Remove(v)
+				fmt.Println("string exist in list b: " + v)
+			} else {
+				resultOnlyInA = append(resultOnlyInA, v)
+				fmt.Println("string not exist in list b: " + v)
+			}
+		}
+		for _, v := range setB.Values() {
+			resultOnlyInB = append(resultOnlyInB, v.(string))
+			fmt.Println("add remaining to result b " + v.(string))
+		}
+
+		sort.Strings(resultOnlyInA)
+		sort.Strings(resultOnlyInB)
+		sort.Strings(resultIntersection)
+
+		result := Result{
+			OnlyInA:      resultOnlyInA,
+			OnlyInB:      resultOnlyInB,
+			Intersection: resultIntersection,
+		}
+		c.JSON(200, result)
 	})
 	_ = router.Run(":8080")
 }
